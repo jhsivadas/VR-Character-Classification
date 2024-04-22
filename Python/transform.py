e5b19423144be11a8e2a6efdd11e577e025f7a36
Jay Sivadas <jhsivadas@live.com>
2024-04-22 13:38:03 -0500

    finished projections and model output ot pickle

--- Python/transform.py
+++ Python/transform.py
@@ -2,12 +2,39 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sympy import Plane, Point, Point3D
+
+
+def project(df):
+    return_df = pd.DataFrame(columns=["x", "y", "z"])
+
+    for index, row in df.iterrows():
+        x = row["controller_right_pos.x"]
+        y = row["controller_right_pos.y"]
+        z = row["controller_right_pos.z"]
+        time  = row["time"]
+
+        p = Point3D(x,y,z)
+
+        plane = Plane(Point3D(1, 1, 0), normal_vector =(0, 0, 1)) 
+        projection = plane.projection(p) 
+        return_df.loc[index] = [projection.x, projection.y, projection.z]
+
+
+
+    return return_df
+
 
 
 def matrix(file_name):
     df = pd.read_csv(file_name, index_col=False)
-    y_values = df['controller_right_pos.y']
-    x_values = df['controller_right_pos.x']
+    df = project(df)
+    print(df)
+
+
+
+    y_values = df['y']
+    x_values = df['x']
 
     min_x = x_values.min()
     max_x = x_values.max()
@@ -34,8 +61,17 @@ def matrix(file_name):
 
         matrix[scaled_y, scaled_x] += 1
 
-    half = np.percentile(matrix, 90)
     max = np.max(matrix)
+    half = .5 * max
+
+    plt.figure(figsize=(15, 8))
+    sns.heatmap(matrix, annot=True, cmap='gray', cbar=False)
+    plt.xticks(range(matrix.shape[1]))
+    plt.yticks(range(matrix.shape[0]))
+    plt.grid(True)
+    plt.show()
+
+  
 
     for row in range(matrix.shape[0]):
         for col in range(matrix.shape[1]):
@@ -43,16 +79,18 @@ def matrix(file_name):
                 matrix[row, col] = 1
             else:
                 matrix[row, col] = matrix[row, col] / max
-    # plt.figure(figsize=(15, 8))
-    # sns.heatmap(matrix, annot=True, cmap='ocean', cbar=False)
-    # plt.xticks(range(matrix.shape[1]))
-    # plt.yticks(range(matrix.shape[0]))
-    # plt.grid(True)
-    # plt.show()
+    plt.figure(figsize=(15, 8))
+    sns.heatmap(matrix, annot=True, cmap='gray', cbar=False)
+    plt.xticks(range(matrix.shape[1]))
+    plt.yticks(range(matrix.shape[0]))
+    plt.grid(True)
+    plt.show()
 
     return matrix
 
-adjust = matrix('DRI_01.csv')
+import os
+print(os.getcwd())
+adjust = matrix('ARC_01.csv')
 transformed_image = np.expand_dims(adjust, axis=0)  # Add batch dimension
 transformed_image = np.expand_dims(transformed_image, axis=-1)  # Add channel dimension
 
