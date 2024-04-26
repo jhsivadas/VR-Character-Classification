a01204dd8a78f530011183d06828ff55995091da
MaxCoby <42662421+MaxCoby@users.noreply.github.com>
2024-04-26 13:52:51 -0500

    S3 bucket uploading done

--- API Tests/Assets/AWS.cs
+++ API Tests/Assets/AWS.cs
@@ -7,27 +7,27 @@ using UnityEngine.Networking;
 using Amazon.S3;
 using Amazon.S3.Model;
 using System;
+using System.IO;
 
 public class AWS : MonoBehaviour
 {
     private string bucketName = "digits-vr";
-    private string keyName = "HOLYCOW.csv"; // Key name under which the file will be saved in the bucket
-    private string filePath = "/Users/max/Desktop/VR-Character-Classification/API Tests/Assets/HOLYCOW.csv"; // Local path to file
+    private string keyName = "HOLYCOW.csv";
+    private string filePath = "/Users/max/Desktop/VR-Character-Classification/API Tests/Assets/HOLYCOW.csv";
+    private string awsCredentialsPath = "/Users/max/Desktop/VR-Character-Classification/API Tests/Assets/aws_credentials.json"; // Path to your aws_credentials.json file
 
     void Start()
     {
         UploadFile();
     }
 
-    public void onRefresh()
-    {
-        Start();
-    }
-
     private void UploadFile()
     {
-        AmazonS3Client s3Client = new AmazonS3Client(awsAccessKeyId, awsSecretAccessKey, Amazon.RegionEndpoint.USEast1);
-        
+        var credentials = ReadAWSCredentials(awsCredentialsPath);
+        if(credentials == null) return;
+
+        AmazonS3Client s3Client = new AmazonS3Client(credentials.AWSAccessKeyId, credentials.AWSSecretKey, Amazon.RegionEndpoint.USEast2);
+
         try
         {
             PutObjectRequest putRequest = new PutObjectRequest
@@ -50,4 +50,25 @@ public class AWS : MonoBehaviour
             Debug.LogError("Unknown encountered on server. Message:'" + e.Message + "'");
         }
     }
+
+    private AWSCredentials ReadAWSCredentials(string credentialsPath)
+    {
+        try
+        {
+            string jsonText = File.ReadAllText(credentialsPath);
+            return JsonConvert.DeserializeObject<AWSCredentials>(jsonText);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to read AWS credentials: " + e.Message);
+            return null;
+        }
+    }
+}
+
+[Serializable]
+public class AWSCredentials
+{
+    public string AWSAccessKeyId;
+    public string AWSSecretKey;
 }
