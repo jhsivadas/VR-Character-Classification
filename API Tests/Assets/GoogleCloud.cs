eff6b11e4c31b29220ba9d7c630ce8fb4e7d0b9b
MaxCoby <42662421+MaxCoby@users.noreply.github.com>
2024-05-15 15:14:04 -0500

    debug weird bug

--- API Tests/Assets/GoogleCloud.cs
+++ API Tests/Assets/GoogleCloud.cs
@@ -182,16 +182,16 @@ public class GoogleCloud : MonoBehaviour
 
                 ilovejayText.text = $"{obj.Updated.Value.ToUniversalTime()}, {lastCheckTime}";
                 
-                // if (obj.Updated.HasValue && obj.Updated.Value.ToUniversalTime() > lastCheckedTime)
-                // {
-                //     responseReceived = true;
-                //     MemoryStream memoryStream = new MemoryStream();
-                //     storageClient.DownloadObject(bucketName, responseName, memoryStream);
-                //     memoryStream.Position = 0;
-                //     StreamReader reader = new StreamReader(memoryStream);
-                //     string fileContents = reader.ReadToEnd();
-                //     ilovejayText.text = current + fileContents;
-                // }
+                if (obj.Updated.HasValue && obj.Updated.Value.ToUniversalTime() > lastCheckedTime)
+                {
+                    responseReceived = true;
+                    MemoryStream memoryStream = new MemoryStream();
+                    storageClient.DownloadObject(bucketName, responseName, memoryStream);
+                    memoryStream.Position = 0;
+                    StreamReader reader = new StreamReader(memoryStream);
+                    string fileContents = reader.ReadToEnd();
+                    ilovejayText.text = current + fileContents;
+                }
             }
             catch (Exception ex)
             {
