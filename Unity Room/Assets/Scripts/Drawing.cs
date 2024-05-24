7fbc8081d7a3778477c9fb74dd28846afe6c98b3
Nik Tolpegin <nikolastolpegin@gmail.com>
2024-05-23 22:13:18 -0500

    minor simplification

--- Unity Room/Assets/Scripts/Drawing.cs
+++ Unity Room/Assets/Scripts/Drawing.cs
@@ -238,13 +238,9 @@ public class Drawing : MonoBehaviour
                 LogAttributes();
 
                 // sort the files in the correct order before adding to the queue
-                var sortedFiles = Directory.GetFiles(uploadFolderPath, "*.csv")
-                           .OrderBy(filePath => 
-                           {
-                               string fileName = Path.GetFileNameWithoutExtension(filePath);
-                               return int.Parse(new string(fileName.Where(char.IsDigit).ToArray()));
-                           })
-                           .ToList();
+                var preSort = Directory.GetFiles(uploadFolderPath, "*.csv");
+                Array.Sort(preSort);
+                var sortedFiles = preSort.ToList();
 
                 foreach (string uploadFilePath in sortedFiles)
                 {
